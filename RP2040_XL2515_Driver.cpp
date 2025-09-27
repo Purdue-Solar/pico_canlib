2cde9f684b9db2a35aa0814ced157fedb63f172d
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-09-27 11:09:56 -0400

    update pins

--- RP2040_XL2515_Driver.cpp
+++ RP2040_XL2515_Driver.cpp
@@ -6,10 +6,11 @@
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
+#define PIN_INT   8
+#define PIN_CS    9
+#define PIN_SCK  10
+#define PIN_MOSI 11
+#define PIN_MISO 12
 
 XL2515_Driver::XL2515_Driver()
 {
