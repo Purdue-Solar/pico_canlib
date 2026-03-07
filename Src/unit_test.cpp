c3f717de7b2743c797a42df63257741de854ad7d
Michael Wu <michaeldmwu@gmail.com>
2026-03-07 11:23:13 -0500

    changed main for test

--- Src/unit_test.cpp
+++ Src/unit_test.cpp
@@ -4,8 +4,11 @@
 #include <stdbool.h>
 #include <string.h>
 
-/*
-int main(void){
+#define TX_OR_RX 0 // tx = 0, rx = 1
+
+#if TX_OR_RX == 0
+int main(void)
+{
     stdio_init_all();
     sleep_ms(1000);
     fprintf(stdout, "Start\n");
@@ -13,33 +16,35 @@ int main(void){
     pico_canlib::status errorCode;
     errorCode = can.init();
     fprintf(stdout, "Init Code %d\n", errorCode);
-    if (errorCode != pico_canlib::status::SUCCESS){
+    if (errorCode != pico_canlib::status::SUCCESS)
+    {
         fprintf(stdout, "Failed Startup\n");
     }
-    uint8_t SOC[8] = {0,0,0,100,0,0,0,0};
+    uint8_t SOC[8] = {0, 0, 0, 100, 0, 0, 0, 0};
     uint8_t status;
     // while (true){
-        errorCode = can.transmitCAN(XL2515::TX_BUFFER_SEL::TX0, artemis_canid::tempAndSOC, false, SOC, 8, XL2515::PRIORITY::Highest);
-        if (errorCode != pico_canlib::status::SUCCESS){
-            fprintf(stdout, "Failed to Transmit. Error Code #%d\n", errorCode);
+    errorCode = can.transmitCAN(XL2515::TX_BUFFER_SEL::TX0, artemis_canid::tempAndSOC, false, SOC, 8, XL2515::PRIORITY::Highest);
+    if (errorCode != pico_canlib::status::SUCCESS)
+    {
+        fprintf(stdout, "Failed to Transmit. Error Code #%d\n", errorCode);
+    }
+    else
+    {
+        fprintf(stdout, "Successfully Transmitted. Code #%d\n", errorCode);
+        asm volatile("nop \n nop \n nop \n nop \n nop \n nop \n");
+        errorCode = can.checkStatus(&status);
+        // if (errorCode & )
+        if (errorCode != pico_canlib::status::SUCCESS)
+        {
+            fprintf(stdout, "Failed to fetch status byte Error Code #%d\n", errorCode);
         }
-        else{
-            fprintf(stdout, "Successfully Transmitted. Code #%d\n", errorCode);
-            asm volatile("nop \n nop \n nop \n nop \n nop \n nop \n");
-            errorCode = can.checkStatus(&status);
-            // if (errorCode & )
-            if (errorCode != pico_canlib::status::SUCCESS){
-                fprintf(stdout, "Failed to fetch status byte Error Code #%d\n", errorCode);
-            }
-            fprintf(stdout, "Status Buffer: %d\n", status);
-        }
-        sleep_ms(1000);
-
-    // }
+        fprintf(stdout, "Status Buffer: %d\n", status);
+    }
+    sleep_ms(1000);
 
     return 0;
-} */
-
+}
+#else
 int main(void)
 {
     stdio_init_all();
@@ -59,7 +64,7 @@ int main(void)
     uint8_t st;
     while (true)
     {
-        can.checkRXStatus(&st);
+        can.checkStatus(&st);
         if (st & 0x01)
         {
             if (can.receiveCAN(st, 0x01, buffer, 4, 8) != pico_canlib::status::SUCCESS)
@@ -74,7 +79,8 @@ int main(void)
                 printf("failed receive rxb1\n");
             }
         }
-        printf("id: %d\n", memcpy(&id, &buffer, 4));
+        memcpy(&id, buffer, 4);
+        printf("id: %d\n", id);
         printf("data: ");
         for (int i = 0; i < 8; i++)
         {
@@ -84,3 +90,4 @@ int main(void)
         sleep_ms(1);
     }
 }
+#endif
