d8ecbb847ddb8f9c2516a4f6981887901eb995e0
Michael Wu <michaeldmwu@gmail.com>
2026-03-07 14:59:44 -0500

    added instructions in readme

--- Src/unit_test.cpp
+++ Src/unit_test.cpp
@@ -12,6 +12,8 @@ int main(void)
     stdio_init_all();
     sleep_ms(1000);
     fprintf(stdout, "Start\n");
+
+    // init canlib
     pico_canlib can = pico_canlib();
     pico_canlib::status errorCode;
     errorCode = can.init();
@@ -20,17 +22,23 @@ int main(void)
     {
         fprintf(stdout, "Failed Startup\n");
     }
+
     // message
     uint8_t SOC[8] = {6, 7, 6, 7, 6, 8, 7, 6};
+    // status, 8 if tx0, 32 if tx1, 128 if tx2
     uint8_t status;
+
+    // main loop
     while (true)
     {
         // transmits to tx buffer 2 in mcp2515
         errorCode = can.transmitCAN(XL2515::TX_BUFFER_SEL::TX2, artemis_canid::tempAndSOC, false, SOC, 8, XL2515::PRIORITY::Highest);
+        // error
         if (errorCode != pico_canlib::status::SUCCESS)
         {
             fprintf(stdout, "Failed to Transmit. Error Code #%d\n", errorCode);
         }
+        // success
         else
         {
             fprintf(stdout, "Successfully Transmitted. Code #%d\n", errorCode);
@@ -45,7 +53,7 @@ int main(void)
             // 8 = tx0, 32 = tx1, 128 = tx2
             fprintf(stdout, "Status Buffer: %d\n", status);
         }
-        sleep_ms(1000);
+        sleep_ms(1);
     }
 
     return 0;
@@ -70,52 +78,6 @@ int main(void)
     uint8_t st;
     while (true)
     {
-        /*
-        errorCode = can.checkStatus(&st);
-        printf("status: %d\n", st);
-        // if (errorCode & )
-        if (errorCode != pico_canlib::status::SUCCESS)
-        {
-            fprintf(stdout, "Failed to fetch status byte Error Code #%d\n", errorCode);
-        }
-        if (st & 0x01)
-        {
-            if (can.receiveCAN(st, 0x00, buffer, 4, 8) != pico_canlib::status::SUCCESS)
-            {
-                printf("failed receive rxb0\n");
-            }
-            else
-            {
-                // first 4 bytes are buffer, byte 0 & 1 are non-extended id, 2-3 are extended
-                printf("id: %d\n", buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3]);
-                printf("dlc: %d\n", buffer[4]);
-                printf("data: ");
-                for (int i = 0; i < 8; i++)
-                {
-                    printf("%d ", buffer[5 + i]);
-                }
-                printf("\n");
-            }
-        }
-        if (st & 0x02)
-        {
-            if (can.receiveCAN(st, 0x01, buffer, 4, 8) != pico_canlib::status::SUCCESS)
-            {
-                printf("failed receive rxb1\n");
-            }
-            else
-            {
-                // first 4 bytes are buffer, byte 0 & 1 are non-extended id, 2-3 are extended
-                printf("id: %d\n", buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3]);
-                printf("dlc: %d\n", buffer[4]);
-                printf("data: ");
-                for (int i = 0; i < 8; i++)
-                {
-                    printf("%d ", buffer[5 + i]);
-                }
-                printf("\n");
-            }
-        } */
         if (can.receiveCAN(buffer, 4, 8) == pico_canlib::status::SUCCESS)
         {
             printf("id: %d\n", buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3]);
