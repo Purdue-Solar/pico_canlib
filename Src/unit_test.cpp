080fcb764f7fbc06fe52b7273f6994c77290c147
Michael Wu <michaeldmwu@gmail.com>
2026-03-28 14:07:01 -0400

    working interrupt?

--- Src/unit_test.cpp
+++ Src/unit_test.cpp
@@ -56,15 +56,17 @@ void CAN_isr();
 void CAN_irq_init();
 
 uint8_t buffer[13];
-bool data_available = false;
+volatile bool data_available = false;
 
 pico_canlib can = pico_canlib();
 
 void CAN_isr()
 {
-    gpio_acknowledge_irq(8, GPIO_IRQ_EDGE_FALL);
-    can.receiveCAN(buffer, 4, 8);
-    data_available = true;
+    if (gpio_get_irq_event_mask(8) & GPIO_IRQ_EDGE_FALL)
+    {
+        gpio_acknowledge_irq(8, GPIO_IRQ_EDGE_FALL);
+        data_available = true;
+    }
 }
 
 void CAN_irq_init()
@@ -72,30 +74,43 @@ void CAN_irq_init()
     // 8 is gpio int
     gpio_init(8);
     gpio_set_dir(8, false);
-    irq_set_exclusive_handler(8, CAN_isr);
+    gpio_pull_up(8);
+    irq_set_exclusive_handler(IO_IRQ_BANK0, CAN_isr);
     gpio_set_irq_enabled(8, GPIO_IRQ_EDGE_FALL, true);
-    irq_set_enabled(8, true);
+    irq_set_enabled(IO_IRQ_BANK0, true);
 }
 
 int main(void)
 {
     stdio_init_all();
-    sleep_ms(1000);
+    sleep_ms(2000);
     fprintf(stdout, "Start\n");
-    pico_canlib::status errorCode;
-    errorCode = can.init();
-    fprintf(stdout, "Init Code %d\n", errorCode);
-    if (errorCode != pico_canlib::status::SUCCESS)
+    if (can.init() != pico_canlib::status::SUCCESS)
     {
-        fprintf(stdout, "Failed Startup\n");
+        printf("CAN Init Failed\n");
     }
 
     CAN_irq_init();
+    printf("irq set up\n");
 
     for (;;)
     {
+        // if (can.receiveCAN(buffer, 4, 8) == pico_canlib::status::SUCCESS)
+        // {
+        //     printf("id: %d\n", buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3]);
+        //     printf("dlc: %d\n", buffer[4]);
+        //     printf("data: ");
+        //     for (int i = 0; i < 8; i++)
+        //     {
+        //         printf("%d ", buffer[5 + i]);
+        //     }
+        //     printf("\n");
+        //     data_available = false;
+        // }
         if (data_available)
         {
+            data_available = false;
+            can.receiveCAN(buffer, 4, 8);
             printf("id: %d\n", buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3]);
             printf("dlc: %d\n", buffer[4]);
             printf("data: ");
@@ -104,7 +119,6 @@ int main(void)
                 printf("%d ", buffer[5 + i]);
             }
             printf("\n");
-            data_available = false;
         }
         sleep_ms(10);
     }
